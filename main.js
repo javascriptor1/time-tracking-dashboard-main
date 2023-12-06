79bc3803098f6da3e21e7425a2756761a5780d0b
Mohammed Fakih <mhd.fakih@gmail.com>
2023-12-06 18:23:29 +0300

    updated code to listen on div instead of button

--- main.js
+++ main.js
@@ -3,22 +3,22 @@ import data from "./data.json" assert { type: "json" };
 
 // grab all three button elements inside div with class [.period-div] with button element
 const periodButtons = document.querySelectorAll(".period-div button");
+const periodButtonsDiv = document.querySelector(".period-div");
 const currentValue = document.querySelectorAll(".data-div p:nth-of-type(2)");
 const previousValue = document.querySelectorAll(".data-div p:nth-of-type(3)");
 
-periodButtons[0].addEventListener("click", (event) => updateValue(event));
-periodButtons[0].addEventListener("touchstart", (event) => updateValue(event));
-periodButtons[1].addEventListener("click", (event) => updateValue(event));
-periodButtons[2].addEventListener("click", (event) => updateValue(event));
+periodButtonsDiv.addEventListener("click", (event) => updateValue(event));
 
 const updateValue = (event) => {
-  removeActiveClass();
-  event.target.classList.add("active");
-  let index = event.target.dataset.index;
-  for (let i = 0; i < currentValue.length; i++) {
-    currentValue[i].textContent = data[i].timeframes[index].current + "hrs";
-    previousValue[i].textContent =
-      "Last Week - " + data[i].timeframes[index].previous + "hrs";
+  if (event.target.tagName === "BUTTON") {
+    removeActiveClass();
+    event.target.classList.add("active");
+    let index = event.target.dataset.index;
+    for (let i = 0; i < currentValue.length; i++) {
+      currentValue[i].textContent = data[i].timeframes[index].current + "hrs";
+      previousValue[i].textContent =
+        "Last Week - " + data[i].timeframes[index].previous + "hrs";
+    }
   }
 };
 
